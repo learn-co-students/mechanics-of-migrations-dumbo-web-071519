3a7b271c612670fc67119017b716bea4536a7a53
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-07-26 08:44:07 -0400

    Done.

--- artist.rb
+++ artist.rb
@@ -1,24 +1,3 @@
-class CreateArtists < ActiveRecord::Migration
 
-
-    def self.create_table
-      sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        title TEXT, 
-        length INTEGER
-        )
-      SQL
-      ActiveRecord::Base.connection.execute(sql)
-    end
-   
-    def change
-      create_table :artists do |t|
-        t.string :name
-        t.string :genre
-        t.integer :age
-        t.string :hometown
-      end
-    end
-  
-  end
+class Artist < ActiveRecord::Base
+end
